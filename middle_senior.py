4579ef1f9b8243ad2f19ad07ef5d3b8a6c1cf09a
matija-k <frankenstein138@gmail.com>
2019-06-11 05:16:47 +0200

    ...

--- middle_senior.py
+++ middle_senior.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 # MiddleSenior Task - NN
 # Author: Matija Knezevic
 # Version: 1.0
@@ -8,30 +6,32 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def main():
+	trademark_symbol = "®"
 
-trademark_symbol = "®"
+	# get keywords into list
+	keywords_list = []
+	with open("Keyword.txt", "r", encoding="utf-8") as keywords_file:
+			for keyword in keywords_file:
+					item = keyword.strip("\r\n")
+					if len(item) > 0:
+							keywords_list.append(item)
 
-# get keywords into list
-keywords_list = []
-with open("Keyword.txt", "r", encoding="utf-8") as keywords_file:
-        for keyword in keywords_file:
-                item = keyword.strip("\r\n")
-                if len(item) > 0:
-                        keywords_list.append(item)
+	# remove duplicates
+	final_list = list(dict.fromkeys(keywords_list))
 
-# remove duplicates
-final_list = list(dict.fromkeys(keywords_list))
+	# read input file
+	with open("Acme_docs.txt", "r", encoding="utf-8") as acme_file:
+			docs = acme_file.read()
 
-# read input file
-with open("Acme_docs.txt", "r", encoding="utf-8") as acme_file:
-        docs = acme_file.read()
+	# replace keywords and write changes
+	for keyword in final_list:
+			docs = docs.replace(keyword, keyword + trademark_symbol)
+			with open("Output_file.txt", "w", encoding="utf-8") as output_file:
+					output_file.write(docs)
 
-# replace keywords and write changes
-for keyword in final_list:
-        docs = docs.replace(keyword, keyword + trademark_symbol)
-        with open("Output_file.txt", "w", encoding="utf-8") as output_file:
-                output_file.write(docs)
 
+main()
 
 if __name__ == "__main__":
 app.run()
