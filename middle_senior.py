a915175a9701e3af3b3a602ca0a4dbf8ec01bae4
matija-k <frankenstein138@gmail.com>
2019-06-11 05:07:53 +0200

    ...

--- middle_senior.py
+++ middle_senior.py
@@ -4,35 +4,34 @@
 # Author: Matija Knezevic
 # Version: 1.0
 
-import flask
-app = flask.Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 @app.route("/")
-
 
 trademark_symbol = "®"
 
 # get keywords into list
 keywords_list = []
 with open("Keyword.txt", "r", encoding="utf-8") as keywords_file:
-	for keyword in keywords_file:
-		item = keyword.strip("\r\n")
-		if len(item) > 0:
-			keywords_list.append(item)
+        for keyword in keywords_file:
+                item = keyword.strip("\r\n")
+                if len(item) > 0:
+                        keywords_list.append(item)
 
 # remove duplicates
-final_list = list(dict.fromkeys(keywords_list)) 
+final_list = list(dict.fromkeys(keywords_list))
 
 # read input file
 with open("Acme_docs.txt", "r", encoding="utf-8") as acme_file:
-	docs = acme_file.read()
+        docs = acme_file.read()
 
 # replace keywords and write changes
 for keyword in final_list:
-	docs = docs.replace(keyword, keyword + trademark_symbol)
-	with open("Output_file.txt", "w", encoding="utf-8") as output_file:
-		output_file.write(docs)
+        docs = docs.replace(keyword, keyword + trademark_symbol)
+        with open("Output_file.txt", "w", encoding="utf-8") as output_file:
+                output_file.write(docs)
 
 
-
-
+if __name__ == "__main__":
+app.run()
